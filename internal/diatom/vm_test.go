e46edceea3a89d24fade703475ea43b97e1ac5a8
eldelto <eldelto77@gmail.com>
2024-02-16 12:53:09 +0100

    Create diatom package

---
+++ internal/diatom/vm_test.go
@@ -0,0 +1,23 @@
+package diatom_test
+
+//import (
+//	"testing"
+//
+//	"github.com/eldelto/core/internal/diatom"
+//)
+//
+//func TestVM(t *testing.T) {
+//  //vm := diatom.NewVM()
+//
+//  testData := []struct {
+//    name string
+//    code string
+//    expectedDataStack []diatom.Word
+//    expectedReturnStack []diatom.Word
+//    wantErr bool
+//  }{
+//    {"exit", "exit", []diatom.Word{}, []diatom.Word{}, false},
+//  }
+//
+//  // TODO: Implement test loop.
+//}
