1385ae61f49027ee380645b0ecd4a7e9f4b4a2c6
eldelto <eldelto77@gmail.com>
2024-04-09 19:06:35 +0200

    Refactor Diatom CLI tools

--- cmd/diatom/cmd/root.go
+++ cmd/diatom/cmd/root.go
@@ -2,10 +2,8 @@ package cmd
 
 import (
 	"bytes"
-	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/eldelto/core/internal/diatom"
@@ -19,62 +17,16 @@ func repl() ([]byte, error) {
 	return dopc, err
 }
 
-func loadProgram(path string) ([]byte, error) {
-	var program []byte
-	needsAssembly := false
-
-	switch {
-	case strings.HasSuffix(path, ".dopc"):
-	case strings.HasSuffix(path, ".dasm"):
-		needsAssembly = true
-	default:
-		return nil, fmt.Errorf("%q is not a supported file format", filepath.Ext(path))
-	}
-
-	in, err := os.ReadFile(path)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read file %q: %w", path, err)
-	}
-	program = in
-
-	if needsAssembly {
-		_, _, dopc, err := diatom.Assemble(bytes.NewBuffer(in))
-		if err != nil {
-			return nil, err
-		}
-		program = dopc
-	}
-
-	return program, err
-}
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "diatom [path]",
+	Use:   "diatom",
 	Args:  cobra.MatchAll(cobra.RangeArgs(0, 1)),
-	Short: "Diatom Virtual Machine",
-	Long: `diatom starts the Diatom virtual machine, loads the file at the given path and
-starts executing.
-
-If path points to a .dopc file it will be executed directly, if it points to a
-.dasm file it will assemble it first (see diatom assemble -h for more
-information).`,
+	Short: "Diatom REPL",
+	Long:  `diatom starts a basic Diatom read-eval-print-loop (REPL).`,
 	Run: func(cmd *cobra.Command, args []string) {
-		var program []byte
-
-		if len(args) < 1 {
-			dopc, err := repl()
-			if err != nil {
-				log.Fatal(err)
-			}
-			program = dopc
-		} else {
-			path := args[0]
-			dopc, err := loadProgram(path)
-			if err != nil {
-				log.Fatal(err)
-			}
-			program = dopc
+		program, err := repl()
+		if err != nil {
+			log.Fatal(err)
 		}
 
 		vm, err := diatom.NewDefaultVM(program)
