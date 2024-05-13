8fd522985deacb1b2a3b781e114f4b774d811a25
eldelto <eldelto77@gmail.com>
2024-05-13 08:23:45 +0200

    More Javascript implementation

--- internal/diatom/diatom.js
+++ internal/diatom/diatom.js
@@ -1,8 +1,6 @@
 const wordSize = 4;
 
 class Stack {
-	// TODO: Implement based on 32 bit typed arrays
-
 	#cursor = 0;
 	#data;
 
@@ -13,7 +11,7 @@ class Stack {
 
 	push(value) {
 		if (this.#cursor+1 >= this.#data.length) {
-			throw `push: stack overflow - cursor: ${this.#cursor}, stack size: ${this.#data.length}`;
+			throw new Error(`push: stack overflow - cursor: ${this.#cursor}, stack size: ${this.#data.length}`);
 		}
 
 		this.#data[this.#cursor] = value;
@@ -23,7 +21,7 @@ class Stack {
 
 	pop() {
 		if (this.#cursor <= 0) {
-			throw `pop: stack underflow - cursor: ${this.#cursor}, stack size: ${this.#data.length}`;
+			throw new Error(`pop: stack underflow - cursor: ${this.#cursor}, stack size: ${this.#data.length}`);
 		}
 
 		this.#cursor--;
@@ -32,36 +30,63 @@ class Stack {
 
 	peek() {
 		if (this.#cursor <= 0) {
-			throw `peek: stack underflow - cursor: ${this.#cursor}, stack size: ${this.#data.length}`;
+			throw new Error(`peek: stack underflow - cursor: ${this.#cursor}, stack size: ${this.#data.length}`);
 		}
 
 		return this.#data[this.#cursor-1];
 	}
 }
 
+//class Input {
+//	// TODO: Implement
+//}
+
+class DiatomVM {
+	static stackSize = 30;
+	static ioBufferSize = 4096;
+	static memorySize = 8192;
+
+	#programCounter = 0;
+	#dataStack = new Stack(this.stackSize);
+	#returnStack = new Stack(this.stackSize);
+	//#inputBuffer = new Input();
+	//input = some element;
+	//output = some element;
+	#memory = new Uint8Array(new ArrayBuffer(this.memorySize));
+
+	load(path) {
+		fetch(path).then(response => {
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
+
+		const program = new Uint8Array(response.arrayBuffer());
+		});
+	}
+	// TODO: Implement
+}
+
+/*
+  How do I want to use that stuff?
+
+  <script src="diatom.js" />
+  <script>
+  const vm = DiatomVM.load("my-script.dia");
+  vm.execute();
+  vm.reset();
+  </script>
+
+  Handling Javascript events would immediately require some sort of
+  event-loop/async programming capabilities but this is out of scope
+  for now.
+*/
+
+
 const s = new Stack(30);
 s.push(11);
 console.log(s.peek());
 console.log(s.pop());
 console.log(s);
-s.pop();
 
-//class Input {
-//	// TODO: Implement
-//}
-//
-//class DiatomVM {
-//	static const stackSize = 30;
-//	static const ioBufferSize = 4096;
-//	static const memorySize = 8192;
-//
-//	programCounter = 0;
-//	dataStack = new Stack(stackDepth);
-//	returnStack = new Stack(stackDepth);
-//	inputBuffer = new Input();
-//	//input = some element;
-//	//output = some element;
-//	memory = new Uint8Array();
-//
-//	// TODO: Implement
-//}
+const vm = new DiatomVM();
+vm.load("my-script.dia");
