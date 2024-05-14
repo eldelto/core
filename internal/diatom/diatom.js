6190c9a8b3e6290ab61f753a6c4aacfd51b31a8b
eldelto <eldelto77@gmail.com>
2024-05-14 08:16:13 +0200

    More Javascript VM implemenation

--- internal/diatom/diatom.js
+++ internal/diatom/diatom.js
@@ -1,3 +1,36 @@
+// Instructions
+const EXIT = 0;
+const NOP = 1;
+const RET = 2;
+const CONST = 3;
+const FETCH = 4;
+const STORE = 5;
+const ADD = 6;
+const SUBTRACT = 7;
+const MULTIPLY = 8;
+const DIVIDE = 9;
+const MODULO = 10;
+const DUP = 11;
+const DROP = 12;
+const SWAP = 13;
+const OVER = 14;
+const CJMP = 15;
+const CALL = 16;
+const SCALL = 17;
+const KEY = 18;
+const EMIT = 19;
+const EQUALS = 20;
+const NOT = 21;
+const AND = 22;
+const OR = 23;
+const LT = 24;
+const GT = 25;
+const RPOP = 26;
+const RPUT = 27;
+const RPEEK = 28;
+const BFETCH = 29;
+const BSTORE = 30;
+
 const wordSize = 4;
 
 class Stack {
@@ -41,27 +74,60 @@ class Stack {
 //	// TODO: Implement
 //}
 
+const stackSize = 30;
+const ioBufferSize = 4096;
+const memorySize = 8192;
+
 class DiatomVM {
-	static stackSize = 30;
-	static ioBufferSize = 4096;
-	static memorySize = 8192;
 
 	#programCounter = 0;
-	#dataStack = new Stack(this.stackSize);
-	#returnStack = new Stack(this.stackSize);
+	#dataStack = new Stack(stackSize);
+	#returnStack = new Stack(stackSize);
 	//#inputBuffer = new Input();
 	//input = some element;
 	//output = some element;
-	#memory = new Uint8Array(new ArrayBuffer(this.memorySize));
+	#memory = new Uint8Array(new ArrayBuffer(memorySize));
 
-	load(path) {
-		fetch(path).then(response => {
-		if (!response.ok) {
-			throw new Error(`HTTP error! Status: ${response.status}`);
+	load(program) {
+		if (program.length > this.#memory.length) {
+			throw new Error(`program length (${program.length} bytes) exceeds available memory (${this.#memory.length} bytes)`);
+			return;
 		}
 
-		const program = new Uint8Array(response.arrayBuffer());
+		this.#memory.set(new Uint8Array(program));
+	}
+
+	loadRemote(path) {
+		fetch(path).then(response => {
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+
+			const program = new Uint8Array(response.arrayBuffer());
 		});
+	}
+
+	execute() {
+		while (true) {
+			const instruction = this.#memory[this.#programCounter];
+			console.log(instruction);
+
+			switch (instruction) {
+			case 0: // EXIT
+				console.log("VM exited normally");
+				return;
+			case 1: // NOP
+				break;
+			case 2: // RET
+				break;
+			case 2: // CONST
+				break;
+			default: 
+				throw new Error(`unknown instruction '${instruction}' at memory address '${this.#programCounter}' - terminating`);
+			}
+
+			this.#programCounter++;
+		}
 	}
 	// TODO: Implement
 }
@@ -88,5 +154,10 @@ console.log(s.peek());
 console.log(s.pop());
 console.log(s);
 
+const program = new Uint8Array(new ArrayBuffer(10));
+program.set([11], 0);
+console.log(program);
+
 const vm = new DiatomVM();
-vm.load("my-script.dia");
+vm.load(program);
+vm.execute();
