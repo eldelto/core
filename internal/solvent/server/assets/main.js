78157f34a1f12278dc32d5feb1c11f92a3279dbf
eldelto <eldelto77@gmail.com>
2024-05-15 18:01:16 +0200

    Improve list editing

--- internal/solvent/server/assets/main.js
+++ internal/solvent/server/assets/main.js
@@ -1,17 +1,35 @@
 const keydownEvent = new Event("keydown");
 
 document.addEventListener("DOMContentLoaded", function() {
+	// Resize textarea to fit the contained content.
 	document.querySelectorAll("textarea.auto-grow")
 		.forEach(e => {
 			e.addEventListener("keydown", autosize);
 			e.dispatchEvent(keydownEvent);
+		});
+
+	// Move cursor to the end of the textarea.
+	document.querySelectorAll("textarea[autofocus]")
+		.forEach(e => {
+			e.selectionStart = e.value.length;
+			setTimeout(() => e.scrollIntoView(false), 0);
+		});
+
+	// Submit form on shift + enter.
+	document.querySelectorAll("form[data-quick-submit]")
+		.forEach(e => {
+			e.addEventListener("keydown", e => {
+				if (e.key == "Enter" && e.shiftKey) {
+				  e.currentTarget.submit();
+				}
+			});
 		});
 });
 
 function autosize(){
 	var el = this;
 	setTimeout(function(){
-		el.style.cssText = 'height:auto; padding:0';
+		//el.style.cssText = 'height:auto; padding:0';
 		// for box-sizing other than "content-box" use:
 		// el.style.cssText = '-moz-box-sizing:content-box';
 		el.style.cssText = 'height:' + el.scrollHeight + 'px';
