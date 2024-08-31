f74e83fc7751563c8d0b7c4d9121480f9283c7fb
eldelto <eldelto77@gmail.com>
2024-08-31 12:42:25 +0200

    Riddle Club: Generate tile map

--- internal/riddle/server/assets/main.js
+++ internal/riddle/server/assets/main.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let grid = document.querySelector(".grid-5x5");
+    let grid = document.querySelector(".grid-5x6");
     grid.addEventListener("click", function (event) {
     let tile = event.target.closest(".tile");
     tile.classList.add("selected")
     })
-
-
 })
--- internal/riddle/server/controller.go
+++ internal/riddle/server/controller.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"math/rand"
 	"net/http"
 
 	"github.com/eldelto/core/internal/web"
@@ -20,8 +21,41 @@ func NewTilesController() *web.Controller {
 	}
 }
 
+const tileCount = 5 * 6
+
+func randomlyAssignTiles() [tileCount]uint {
+	numbers := [tileCount]uint{}
+	for i := range numbers {
+		numbers[i] = uint(i % (tileCount / 2))
+	}
+
+	for i := range numbers {
+		n := numbers[i]
+		j := rand.Intn(tileCount)
+		numbers[i] = numbers[j]
+		numbers[j] = n
+	}
+
+	return numbers
+}
+
+type tileMap struct {
+	Center [tileCount]uint
+	Mid    [tileCount]uint
+	Outer  [tileCount]uint
+}
+
+func randomTileMap() tileMap {
+	return tileMap{
+		Center: randomlyAssignTiles(),
+		Mid:    randomlyAssignTiles(),
+		Outer:  randomlyAssignTiles(),
+	}
+}
+
 func getTiles() web.Handler {
 	return func(w http.ResponseWriter, r *http.Request) error {
-		return tilesTemplate.Execute(w, nil)
+		tileMap := randomTileMap()
+		return tilesTemplate.Execute(w, tileMap)
 	}
 }
