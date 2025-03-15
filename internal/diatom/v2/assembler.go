1759083424bbda39e5ecc1cdeb03dcefaded237c
eldelto <eldelto77@gmail.com>
2025-03-15 13:58:12 +0100

    Diatom: Start V2 implementation

---
+++ internal/diatom/v2/assembler.go
@@ -0,0 +1,604 @@
+package diatom
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"math"
+	"strconv"
+)
+
+type pos struct {
+	line   uint
+	column uint
+}
+
+type tokenScanner struct {
+	pos   pos
+	token []byte
+	r     io.Reader
+}
+
+func (s *tokenScanner) scan() error {
+
+	var buffer [1]byte
+	var err error
+	for {
+		_, err = s.r.Read(buffer[:])
+		if err != nil {
+			if errors.Is(err, io.EOF) && len(s.token) > 0 {
+				return nil
+			}
+			return err
+		}
+		b := buffer[0]
+
+		s.pos.column++
+
+		if b < 33 {
+			if b == '\n' {
+				s.pos.line++
+				s.pos.column = 0
+			}
+			// Skip initial whitespace else finish token.
+			if len(s.token) == 0 {
+				continue
+			} else {
+				break
+			}
+		}
+
+		s.token = append(s.token, b)
+	}
+
+	return nil
+}
+
+func (s *tokenScanner) Token() (string, error) {
+	if len(s.token) > 0 {
+		return string(s.token), nil
+	}
+
+	err := s.scan()
+	return string(s.token), err
+}
+
+func (s *tokenScanner) Consume() {
+	s.token = s.token[:0]
+}
+
+func (s *tokenScanner) Consumed() bool {
+	return len(s.token) == 0
+}
+
+type assembler struct {
+	scanner      *tokenScanner
+	writer       io.Writer
+	lastWordName string
+	labels       map[string]Word
+}
+
+func newAssembler(r io.Reader, w io.Writer) *assembler {
+	return &assembler{
+		scanner: &tokenScanner{
+			token: []byte{},
+			r:     r,
+		},
+		writer: w,
+		labels: map[string]Word{},
+	}
+}
+
+func scanError(asm *assembler, err error) error {
+	return fmt.Errorf("line %d, pos %d: %w",
+		asm.scanner.pos.line,
+		asm.scanner.pos.column,
+		err)
+}
+
+func doUntil(asm *assembler, delimiter string, f func(asm *assembler) error) error {
+	for {
+		token, err := asm.scanner.Token()
+		if err != nil {
+			return fmt.Errorf("expected delimiter %q", delimiter)
+		}
+
+		if token == delimiter {
+			asm.scanner.Consume()
+			return nil
+		}
+
+		if err := f(asm); err != nil {
+			return err
+		}
+	}
+}
+
+func dropToken(asm *assembler) error {
+	asm.scanner.Consume()
+	return nil
+}
+
+func passTokenThrough(asm *assembler) error {
+	token, err := asm.scanner.Token()
+	if err != nil {
+		return err
+	}
+	asm.scanner.Consume()
+
+	_, err = fmt.Fprintln(asm.writer, token)
+	return err
+}
+
+func expectToken[T any](asm *assembler, f func(token string) (T, error)) (T, error) {
+	var result T
+	token, err := asm.scanner.Token()
+	if err != nil {
+		return result, fmt.Errorf("unexpected end of expression")
+	}
+
+	result, err = f(token)
+	if err != nil {
+		return result, err
+	}
+
+	asm.scanner.Consume()
+	return result, nil
+}
+
+func match(want string) func(string) (string, error) {
+	return func(token string) (string, error) {
+		if token != want {
+			return "", fmt.Errorf("expected %q but got %q", want, token)
+		}
+
+		return token, nil
+	}
+}
+
+func identifier(token string) (string, error) {
+	if token[0] == '.' && len(token) > 1 {
+		return "", fmt.Errorf("expected non-macro identifier but got %q", token)
+	}
+
+	if len(token) > 127 {
+		return "", fmt.Errorf("%q exceeds the maximum identifier length of %d characters", token, maxTokenLen)
+	}
+
+	return token, nil
+}
+
+func positiveNumber(token string) (byte, error) {
+	number, err := strconv.Atoi(token)
+	if err != nil {
+		return 0, fmt.Errorf("%q is not a valid positive number", token)
+	}
+
+	if number > math.MaxUint8 {
+		return 0, fmt.Errorf("%d exceeds the maximum allowed value of %d", number, math.MaxUint8)
+	}
+
+	if number < 0 {
+		return 0, fmt.Errorf("%q exceeds the minimum allowed value of 0", token)
+	}
+
+	return byte(number), nil
+}
+
+func anyOf(asm *assembler, parsers ...func(asm *assembler) error) error {
+	for _, parser := range parsers {
+		if err := parser(asm); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func expectEither(asm *assembler, parsers ...func(asm *assembler) error) func(asm *assembler) error {
+	return func(asm *assembler) error {
+		for _, parser := range parsers {
+			if err := parser(asm); err != nil {
+				return err
+			}
+
+			if asm.scanner.Consumed() {
+				return nil
+			}
+		}
+
+		token, err := asm.scanner.Token()
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("expected expression but got %q", token)
+	}
+}
+
+func expandComment(asm *assembler) error {
+	token, err := asm.scanner.Token()
+	if err != nil {
+		return err
+	}
+
+	if token != "(" {
+		return nil
+	}
+	asm.scanner.Consume()
+
+	return doUntil(asm, ")", dropToken)
+}
+
+func expandNumber(asm *assembler) error {
+	token, err := asm.scanner.Token()
+	if err != nil {
+		return err
+	}
+
+	number, err := strconv.ParseInt(token, 10, 32)
+	if err != nil {
+		return nil
+	}
+	asm.scanner.Consume()
+
+	return writeAsBytes(asm.writer, Word(number))
+}
+
+func writeDictionaryHeader(asm *assembler, name string, immediate bool) error {
+	// Label of the dictionary entry.
+	if _, err := fmt.Fprintln(asm.writer, ":"+name); err != nil {
+		return err
+	}
+
+	// TODO: Make this only a single byte and enforce that a word can
+	// only be 100 words long.
+	
+	// The pointer to the previous word.
+	if asm.lastWordName == "" {
+		if err := writeAsBytes(asm.writer, 0); err != nil {
+			return err
+		}
+	} else {
+		if _, err := fmt.Fprintln(asm.writer, "r@_dict-"+asm.lastWordName); err != nil {
+			return err
+		}
+	}
+	asm.lastWordName = name
+
+	// Lenght and characters of the current word's name.
+	nameLen := len(name)
+	if immediate {
+		nameLen |= 128
+	}
+	if _, err := fmt.Fprintf(asm.writer, "%d", nameLen); err != nil {
+		return err
+	}
+
+	for _, c := range []byte(name) {
+		if _, err := fmt.Fprintf(asm.writer, " %d", c); err != nil {
+			return err
+		}
+	}
+	_, err := fmt.Fprintln(asm.writer)
+	return err
+}
+
+func expandCodeWord(asm *assembler) error {
+	token, err := asm.scanner.Token()
+	if err != nil {
+		return err
+	}
+
+	immediate := false
+
+	switch {
+	case token == ".codeword":
+	case token == ".immediate-codeword":
+		immediate = true
+	default:
+		return nil
+	}
+	asm.scanner.Consume()
+
+	name, err := expectToken(asm, identifier)
+	if err != nil {
+		return err
+	}
+
+	if err := writeDictionaryHeader(asm, name, immediate); err != nil {
+		return err
+	}
+
+	// Label of the first code pointer.
+	if _, err := fmt.Fprintln(asm.writer, ":"+name); err != nil {
+		return err
+	}
+
+	if err := doUntil(asm, ".end", expectEither(asm,
+		expandNumber,
+		passTokenThrough,
+	)); err != nil {
+		return err
+	}
+
+	// Returning from the word.
+	_, err = fmt.Fprintln(asm.writer, "ret")
+	return err
+}
+
+func expandVar(asm *assembler) error {
+	token, err := asm.scanner.Token()
+	if err != nil {
+		return err
+	}
+
+	if token != ".var" {
+		return nil
+	}
+	asm.scanner.Consume()
+
+	name, err := expectToken(asm, identifier)
+	if err != nil {
+		return err
+	}
+
+	size, err := expectToken(asm, positiveNumber)
+	if err != nil {
+		return err
+	}
+
+	if _, err := expectToken(asm, match(".end")); err != nil {
+		return err
+	}
+
+	if err := writeDictionaryHeader(asm, name, false); err != nil {
+		return err
+	}
+
+	// Label of the first code pointer.
+	if _, err := fmt.Fprintln(asm.writer, ":"+name); err != nil {
+		return err
+	}
+
+	// Code for storing the variable address on the stack.
+	if _, err := fmt.Fprintf(asm.writer, "const\n@_var-%s\nret\n", name); err != nil {
+		return err
+	}
+
+	// Label and storage for the actual variable.
+	if _, err := fmt.Fprintln(asm.writer, ":_var-"+name); err != nil {
+		return err
+	}
+	for i := 0; i < int(size); i++ {
+		if _, err := fmt.Fprintln(asm.writer, "0"); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func expandMacros(asm *assembler) error {
+	pos := asm.scanner.pos
+	if err := anyOf(asm,
+		expandComment,
+		expandCodeWord,
+		expandVar,
+		expandNumber,
+	); err != nil {
+		return err
+	}
+
+	// Pass token through if no previous function could make progress.
+	if pos != asm.scanner.pos {
+		return nil
+	}
+
+	if err := passTokenThrough(asm); err != nil {
+		return err
+	}
+
+	asm.scanner.Consume()
+	return nil
+}
+
+func ExpandMacros(r io.Reader, w io.Writer) error {
+	asm := newAssembler(r, w)
+
+	for {
+		if err := expandMacros(asm); err != nil {
+			if errors.Is(err, io.EOF) {
+				return nil
+			}
+
+			return scanError(asm, err)
+		}
+	}
+}
+
+func readLabels(asm *assembler) error {
+	var address Word = 0
+
+	for {
+		token, err := asm.scanner.Token()
+		if err != nil {
+			return err
+		}
+
+		first := token[0]
+		longEnough := len(token) > 1
+
+		switch {
+		case longEnough && first == ':':
+			label := token[1:]
+			prevAddress, ok := asm.labels[label]
+			if ok {
+				return fmt.Errorf("label %q already declared at address '%d'",
+					label, prevAddress)
+			}
+			asm.labels[label] = address
+		case longEnough && first == '@':
+			address += WordSize
+		case first == '(':
+			if err := expandComment(asm); err != nil {
+				return err
+			}
+			continue
+		default:
+			address++
+		}
+
+		if err := passTokenThrough(asm); err != nil {
+			return err
+		}
+	}
+}
+
+func resolveLabel(asm *assembler) error {
+	token, err := asm.scanner.Token()
+	if err != nil {
+		return err
+	}
+
+	if len(token) < 2 {
+		return nil
+	}
+	label := token[1:]
+
+	switch token[0] {
+	case ':':
+		address, ok := asm.labels[label]
+		if !ok {
+			return fmt.Errorf("no previous declaration of label %q found", label)
+		}
+
+		if _, err := fmt.Fprintf(asm.writer, "( ':%s' at address '%d' )\n", label, address); err != nil {
+			return err
+		}
+	case '@':
+		address, ok := asm.labels[label]
+		if !ok {
+			return fmt.Errorf("no declaration of label %q found", label)
+		}
+
+		if _, err := fmt.Fprintf(asm.writer, "( '@%s' at address '%d' )\n", label, address); err != nil {
+			return err
+		}
+
+		if err := writeAsBytes(asm.writer, address); err != nil {
+			return err
+		}
+	default:
+		return nil
+	}
+
+	asm.scanner.Consume()
+	return nil
+}
+
+func ResolveLabels(r io.Reader, w io.Writer) error {
+	labelBuf := &bytes.Buffer{}
+	asm := newAssembler(r, labelBuf)
+
+	if err := readLabels(asm); err != nil && !errors.Is(err, io.EOF) {
+		return scanError(asm, err)
+	}
+
+	labels := asm.labels
+	asm = newAssembler(labelBuf, w)
+	asm.labels = labels
+	for {
+		if err := expectEither(asm, expandComment, resolveLabel, passTokenThrough)(asm); err != nil {
+			if errors.Is(err, io.EOF) {
+				return nil
+			}
+
+			return scanError(asm, err)
+		}
+	}
+}
+
+func writeNumber(asm *assembler) error {
+	token, err := asm.scanner.Token()
+	if err != nil {
+		return err
+	}
+
+	if _, err := strconv.Atoi(token); err != nil {
+		return nil
+	}
+	asm.scanner.Consume()
+
+	opcode, err := positiveNumber(token)
+	if err != nil {
+		return err
+	}
+
+	_, err = asm.writer.Write([]byte{opcode})
+	if err != nil {
+		return fmt.Errorf("failed to write opcode for %q: %w", token, err)
+	}
+
+	return nil
+}
+
+func resolveInstruction(asm *assembler) error {
+	token, err := asm.scanner.Token()
+	if err != nil {
+		return err
+	}
+	asm.scanner.Consume()
+
+	opcode, ok := instructions[token]
+	if !ok {
+		return fmt.Errorf("%q is not a valid instruction", token)
+	}
+
+	_, err = asm.writer.Write([]byte{opcode})
+	if err != nil {
+		return fmt.Errorf("failed to write opcode for %q: %w", token, err)
+	}
+
+	return nil
+}
+
+func GenerateMachineCode(r io.Reader, w io.Writer) error {
+	asm := newAssembler(r, w)
+
+	for {
+		if err := expectEither(asm,
+			expandComment,
+			writeNumber,
+			resolveInstruction,
+		)(asm); err != nil {
+			if errors.Is(err, io.EOF) {
+				return nil
+			}
+
+			return scanError(asm, err)
+		}
+	}
+}
+
+func Assemble(r io.Reader) (dexp, dins string, dopc []byte, err error) {
+	out := bytes.Buffer{}
+	if err := ExpandMacros(r, &out); err != nil {
+		return "", "", nil, err
+	}
+	dexp = out.String()
+
+	out.Reset()
+	if err := ResolveLabels(bytes.NewBufferString(dexp), &out); err != nil {
+		return "", "", nil, err
+	}
+	dins = out.String()
+
+	out.Reset()
+	if err := GenerateMachineCode(bytes.NewBufferString(dins), &out); err != nil {
+		return "", "", nil, err
+	}
+	dopc = out.Bytes()
+
+	return
+}
---
+++ internal/diatom/v2/vm.go
@@ -0,0 +1,675 @@
+package diatom
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/eldelto/core/internal/collections"
+)
+
+const (
+	StackSize    = 20
+	IOBufferSize = 4096
+	MemorySize   = 8192
+)
+
+type Stack struct {
+	cursor int
+	data   [StackSize]Word
+}
+
+func (s *Stack) Push(value Word) error {
+	if s.cursor+1 >= len(s.data) {
+		return fmt.Errorf("push: stack overflow - cursor: %d, stack size: %d",
+			s.cursor, len(s.data))
+	}
+
+	s.data[s.cursor] = value
+	s.cursor++
+	return nil
+}
+
+func (s *Stack) Pop() (Word, error) {
+	if s.cursor <= 0 {
+		return 0, fmt.Errorf("pop: stack underflow - cursor: %d, stack size: %d",
+			s.cursor, len(s.data))
+	}
+
+	s.cursor--
+	return s.data[s.cursor], nil
+}
+
+func (s *Stack) Peek() (Word, error) {
+	if s.cursor <= 0 {
+		return 0, fmt.Errorf("peek: stack underflow - cursor: %d, stack size: %d",
+			s.cursor, len(s.data))
+	}
+
+	return s.data[s.cursor-1], nil
+}
+
+func (s *Stack) String() string {
+	b := strings.Builder{}
+
+	for _, w := range s.data[:s.cursor] {
+		b.WriteString(strconv.Itoa(int(w)))
+		b.WriteString(", ")
+	}
+
+	return b.String()
+}
+
+type Input struct {
+	cursor int
+	len    int
+	buffer [IOBufferSize]byte
+}
+
+func (i *Input) nextChar(r io.Reader) (byte, error) {
+	if i.cursor >= i.len {
+		read, err := io.ReadAtLeast(r, i.buffer[:], 1)
+		if read == 0 || err != nil {
+			return 0, fmt.Errorf("failed to read from input: %w", err)
+		}
+
+		i.len = read
+		i.cursor = 0
+	}
+
+	b := i.buffer[i.cursor]
+	i.cursor++
+
+	return b, nil
+}
+
+type traceEntry struct {
+	programCounter Word
+	instruction    byte
+	dataStack      Stack
+	returnStack    Stack
+}
+
+type ExtensionFunc func(vm *VM)
+
+type Extension struct {
+	Addr      uint16
+	Functions []ExtensionFunc
+	Name      string
+}
+
+type VM struct {
+	programCounter Word
+	dataStack      Stack
+	returnStack    Stack
+	inputBuffer    Input
+	input          io.Reader
+	output         io.Writer
+	memory         [MemorySize]byte
+	executionTrace collections.RingBuffer[traceEntry]
+	extensions     map[Word]ExtensionFunc
+}
+
+func NewVM(program []byte, input io.Reader, output io.Writer) (*VM, error) {
+	programLen := len(program)
+	if programLen > MemorySize {
+		return nil, fmt.Errorf("program length (%d bytes) exceeds available memory (%d bytes)",
+			programLen, MemorySize)
+	}
+
+	vm := VM{
+		input:          input,
+		output:         output,
+		executionTrace: collections.NewRingBuffer[traceEntry](StackSize),
+		extensions:     map[Word]ExtensionFunc{},
+	}
+	copy(vm.memory[:], program)
+
+	return &vm, nil
+}
+
+func NewDefaultVM(program []byte) (*VM, error) {
+	return NewVM(program, os.Stdin, os.Stdout)
+}
+
+func (vm *VM) RegisterExtension(ext Extension) error {
+	moduleAddr := Word(ext.Addr) << 16
+	for funcAddr, extFunc := range ext.Functions {
+		addr := moduleAddr | Word(funcAddr)
+		if _, ok := vm.extensions[addr]; ok {
+			return fmt.Errorf("extension %q: function already exists at address '%d'",
+				ext.Name, addr)
+		}
+
+		vm.extensions[addr] = extFunc
+	}
+
+	return nil
+}
+
+func (vm *VM) appendTraceEntry(instruction byte) {
+	entry := traceEntry{
+		programCounter: vm.programCounter,
+		instruction:    instruction,
+		dataStack:      vm.dataStack,
+		returnStack:    vm.returnStack,
+	}
+
+	vm.executionTrace.Append(entry)
+}
+
+func (vm *VM) validateMemoryAccess(addr Word) error {
+	if addr >= Word(len(vm.memory)) || addr < 0 {
+		return fmt.Errorf("out of bound memory access: programCounter=%d address=%d",
+			vm.programCounter, addr)
+	}
+
+	return nil
+}
+
+func (vm *VM) fetchByte(addr Word) (byte, error) {
+	if err := vm.validateMemoryAccess(addr); err != nil {
+		return 0, err
+	}
+
+	return vm.memory[addr], nil
+}
+
+func (vm *VM) storeByte(addr Word, b byte) error {
+	if err := vm.validateMemoryAccess(addr); err != nil {
+		return err
+	}
+
+	vm.memory[addr] = b
+	return nil
+}
+
+func (vm *VM) fetchWord(addr Word) (Word, error) {
+	var w Word
+	for i := 0; i < WordSize; i++ {
+		b, err := vm.fetchByte(addr + Word(i))
+		if err != nil {
+			return w, err
+		}
+
+		shift := (WordSize - (i + 1)) * 8
+		w = w | (Word(b) << shift)
+	}
+
+	return w, nil
+}
+
+func (vm *VM) storeWord(addr Word, w Word) error {
+	bytes := wordToBytes(w)
+	for i := 0; i < WordSize; i++ {
+		if err := vm.storeByte(addr+(WordSize-(Word(i)+1)), bytes[i]); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (vm *VM) key() (byte, error) {
+	return vm.inputBuffer.nextChar(vm.input)
+}
+
+func (vm *VM) emit(b byte) error {
+	if _, err := vm.output.Write([]byte{b}); err != nil {
+		return fmt.Errorf("failed to write %q to the VM output: %w", b, err)
+	}
+
+	return nil
+}
+
+func (vm *VM) dumpMemory(fileName string, endAddr Word) error {
+	if err := os.WriteFile(fileName, vm.memory[:endAddr], 0644); err != nil {
+		return fmt.Errorf("failed to dump memory addresses 0 - %d to file %q",
+			endAddr, fileName)
+	}
+
+	return nil
+}
+
+func boolToWord(b bool) Word {
+	if b {
+		return -1
+	}
+
+	return 0
+}
+
+func add(a, b Word) Word {
+	la, lb := int64(a), int64(b)
+	lc := la + lb
+
+	if lc > WordMax {
+		return WordMax
+	} else if lc < WordMin {
+		return WordMin
+	} else {
+		return Word(lc)
+	}
+}
+
+func subtract(a, b Word) Word {
+	la, lb := int64(a), int64(b)
+	lc := la - lb
+
+	if lc > WordMax {
+		return WordMax
+	} else if lc < WordMin {
+		return WordMin
+	} else {
+		return Word(lc)
+	}
+}
+
+func multiply(a, b Word) Word {
+	la, lb := int64(a), int64(b)
+	lc := la * lb
+
+	if lc > WordMax {
+		return WordMax
+	} else if lc < WordMin {
+		return WordMin
+	} else {
+		return Word(lc)
+	}
+}
+
+func (vm *VM) execute() error {
+	for {
+		instruction := vm.memory[vm.programCounter]
+		vm.appendTraceEntry(instruction)
+
+		switch instruction {
+		case ABORT:
+			return fmt.Errorf("tried to execute uninitialized memory address at %d - aborting", vm.programCounter)
+		case EXIT:
+			return nil
+		case RET:
+			addr, err := vm.returnStack.Pop()
+			if err != nil {
+				return err
+			}
+			vm.programCounter = addr
+			continue
+		case JMP:
+			vm.programCounter++
+			addr, err := vm.fetchWord(vm.programCounter)
+				if err != nil {
+					return err
+				}
+			vm.programCounter = addr
+			continue
+		case CJMP:
+			vm.programCounter++
+			conditional, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+
+			if conditional == -1 {
+				vm.programCounter, err = vm.fetchWord(vm.programCounter)
+				if err != nil {
+					return err
+				}
+			} else {
+				vm.programCounter += WordSize
+			}
+			continue
+		case CALL:
+			vm.programCounter++
+			if err := vm.returnStack.Push(vm.programCounter + WordSize); err != nil {
+				return err
+			}
+
+			target, err := vm.fetchWord(vm.programCounter)
+			if err != nil {
+				return err
+			}
+			vm.programCounter = target
+			continue
+		case EXCALL:
+			addr, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			extFunc, ok := vm.extensions[addr]
+			if !ok {
+				return fmt.Errorf("extension function at address '%d' not found",
+					addr)
+			}
+			extFunc(vm)
+
+			
+		case CONST:
+			vm.programCounter++
+			w, err := vm.fetchWord(vm.programCounter)
+			if err != nil {
+				return err
+			}
+
+			if err := vm.dataStack.Push(w); err != nil {
+				return err
+			}
+
+			vm.programCounter += WordSize
+			continue
+		case DUP:
+			a, err := vm.dataStack.Peek()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(a); err != nil {
+				return err
+			}
+		case DROP:
+			if _, err := vm.dataStack.Pop(); err != nil {
+				return err
+			}
+		case SWAP:
+			a, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			b, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(a); err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(b); err != nil {
+				return err
+			}
+		case OVER:
+			a, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			b, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(b); err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(a); err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(b); err != nil {
+				return err
+			}
+		case RPOP:
+			a, err := vm.returnStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(a); err != nil {
+				return err
+			}
+		case RPUSH:
+			a, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.returnStack.Push(a); err != nil {
+				return err
+			}
+			
+		case STORE:
+			addr, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			value, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.storeWord(addr, value); err != nil {
+				return err
+			}
+		case FETCH:
+			addr, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			w, err := vm.fetchWord(addr)
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(w); err != nil {
+				return err
+			}
+		case BSTORE:
+			addr, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			value, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.storeByte(addr, byte(value)); err != nil {
+				return err
+			}
+		case BFETCH:
+			addr, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			b, err := vm.fetchByte(addr)
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(Word(b)); err != nil {
+				return err
+			}
+
+		case ADD:
+			a, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			b, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(add(b, a)); err != nil {
+				return err
+			}
+		case SUB:
+			a, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			b, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(subtract(b, a)); err != nil {
+				return err
+			}
+		case MULT:
+			a, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			b, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(multiply(b, a)); err != nil {
+				return err
+			}
+		case DIV:
+			a, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			b, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(b / a); err != nil {
+				return err
+			}
+		case MOD:
+			a, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			b, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(b % a); err != nil {
+				return err
+			}
+
+		case EQ:
+			a, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			b, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(boolToWord(b == a)); err != nil {
+				return err
+			}
+		case NOT:
+			a, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(^a); err != nil {
+				return err
+			}
+		case AND:
+			a, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			b, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(b & a); err != nil {
+				return err
+			}
+		case OR:
+			a, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			b, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(b | a); err != nil {
+				return err
+			}
+		case LT:
+			a, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			b, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(boolToWord(b < a)); err != nil {
+				return err
+			}
+		case GT:
+			a, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			b, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dataStack.Push(boolToWord(a < b)); err != nil {
+				return err
+			}
+
+					case KEY:
+			b, err := vm.key()
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					return nil
+				}
+				return err
+			}
+			if err := vm.dataStack.Push(Word(b)); err != nil {
+				return err
+			}
+		case EMIT:
+			value, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.emit(byte(value)); err != nil {
+				return err
+			}
+		case DUMP:
+			endAddr, err := vm.dataStack.Pop()
+			if err != nil {
+				return err
+			}
+			if err := vm.dumpMemory("dump.dopc", endAddr); err != nil {
+				return err
+			}
+		default:
+			return fmt.Errorf("unknown instruction '%d' at memory address '%d' - terminating",
+				instruction, vm.programCounter)
+		}
+
+		vm.programCounter++
+	}
+}
+
+func (vm *VM) StackTrace() string {
+	b := strings.Builder{}
+
+	trace := vm.executionTrace.Slice()
+	slices.Reverse(trace)
+
+	for _, entry := range trace {
+		b.WriteString("Program counter: ")
+		b.WriteString(strconv.FormatInt(int64(entry.programCounter), 10))
+
+		b.WriteString("  Instruction: ")
+		b.WriteString(instructionFromOpcode(entry.instruction))
+		b.WriteRune('\n')
+
+		b.WriteString("Data stack: ")
+		b.WriteString(entry.dataStack.String())
+		b.WriteRune('\n')
+
+		b.WriteString("Return stack: ")
+		b.WriteString(entry.returnStack.String())
+		b.WriteRune('\n')
+
+		b.WriteRune('\n')
+	}
+
+	return b.String()
+}
+
+func (vm *VM) Execute() error {
+	if err := vm.execute(); err != nil {
+		return fmt.Errorf("%w:\n\n%s", err, vm.StackTrace())
+	}
+
+	return nil
+}
